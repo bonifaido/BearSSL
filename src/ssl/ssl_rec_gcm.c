2e19e0f812329e97ed54f31301043829b8890f3a
Balint Molnar <balintmolnar91@gmail.com>
2024-03-21 12:25:17 +0100

    Fix key size error

--- src/ssl/ssl_rec_gcm.c
+++ src/ssl/ssl_rec_gcm.c
@@ -42,7 +42,7 @@ gen_gcm_init(br_sslrec_gcm_context *cc,
 	bc_impl->init(&cc->bc.vtable, key, key_len);
 	cc->gh = gh_impl;
 	memcpy(cc->iv, iv, sizeof cc->iv);
-	memcpy(cc->key, key, sizeof key_len);
+	memcpy(cc->key, key, key_len);
 	memset(cc->h, 0, sizeof cc->h);
 	memset(tmp, 0, sizeof tmp);
 	bc_impl->run(&cc->bc.vtable, tmp, 0, cc->h, sizeof cc->h);
